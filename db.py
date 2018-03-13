72ab1f4b88b19b2956d2784ea223e6220a8c8e05
Andres Garagiola <garagiol@ar.ibm.com>
2018-03-12 21:39:20 -0300

    Usa un volumen para los datos de postgres.

--- db.py
+++ db.py
@@ -7,15 +7,10 @@ SCHEMA =  os.getenv('DEFAULT_SCHEMA', 'public')
 
 # SQL STATEMENTS
 EXISTS_TABLE = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = '{}' AND table_name = '{}');"
-<<<<<<< HEAD
 CREATE_TABLE = "CREATE TABLE {}.personas (firstname varchar(255), lastname varchar(255), username1 varchar(255), username2 varchar(255));".format(SCHEMA)
-INSERT_ROW = "INSERT INTO {}.personas (firstname, lastname, username1, username2) values(%s, %s, %s, %s);".format(SCHEMA)
-=======
-CREATE_TABLE = "CREATE TABLE personas (firstname varchar(255), lastname varchar(255), username1 varchar(255), username2 varchar(255));"
 INSERT_QUERY = "INSERT INTO {}.personas (firstname, lastname, username1, username2) values(%s, %s, %s, %s);".format(SCHEMA)
 SELECT_QUERY = "SELECT * FROM {}.personas;".format(SCHEMA)
 
->>>>>>> a2ea151... Agrega servicio para listar personas.
 
 DSN = "dbname={} host={} port={} user={}".format(
     os.getenv('DATABASE', 'postgres'),
@@ -44,30 +39,24 @@ def createTables():
 def insertPersona(firstname, lastname, username1, username2):
     conn = createConnection()
     cur = conn.cursor()
-<<<<<<< HEAD
     try:
         params = [firstname, lastname, username1, username2]
-        cur.execute(INSERT_ROW, params)
+        cur.execute(INSERT_QUERY, params)
         conn.commit()
     finally:
         cur.close()
         conn.close()
-=======
-    params = [firstname, lastname, username1, username2]
-    cur.execute(INSERT_QUERY, params)
-    conn.commit()
-    cur.close()
-    conn.close()
->>>>>>> a2ea151... Agrega servicio para listar personas.
 
 def listarPersonas():
     conn = createConnection()
     cur = conn.cursor()
-    cur.execute(SELECT_QUERY)
-    personas = cur.fetchall()
-    cur.close()
-    conn.close()
-    return personas
+    try:
+        cur.execute(SELECT_QUERY)
+        personas = cur.fetchall()    
+        return personas
+    finally:
+        cur.close()
+        conn.close()
 
 def init():
     wait_time = eval(os.getenv('WAIT_DB', '0'))
